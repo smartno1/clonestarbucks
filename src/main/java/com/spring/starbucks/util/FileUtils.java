ce91f65e7fac98359411156e6e519a7f6eb07062
smartno1 <smartel@daum.net>
2023-02-17 14:51:07 +0900

    123

--- src/main/java/com/spring/starbucks/util/FileUtils.java
+++ src/main/java/com/spring/starbucks/util/FileUtils.java
@@ -1,4 +1,4 @@
-package com.spring.starbucks.common;
+package com.spring.starbucks.util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
